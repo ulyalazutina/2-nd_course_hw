4b2fd5076a093d816114d1cd86e50bfa5c2297db
Ulyana Lazutina <ulyalazutina@yandex.ru>
2023-09-13 11:32:04 +0300

    changed the 5th task

--- js/hw-3.js
+++ js/hw-3.js
@@ -24,41 +24,17 @@ alert(Number(a) + Number(b));
 //Задание 5
 let monthNumber = prompt('Введите номер месяца');
 switch (monthNumber) {
-    case '1':
+    case '1': case '2': case '12':
         alert('Это зима');
         break;
-    case '2':
-        alert('Это зима');
-        break;
-    case '3':
+    case '3': case '4': case '5':
         alert('Это весна');
         break;
-    case '4':
-        alert('Это весна');
-        break;
-    case '5':
-        alert('Это весна');
-        break;
-    case '6':
+    case '6': case '7': case '8':
         alert('Это лето');
         break;
-    case '7':
-        alert('Это лето');
-        break;
-    case '8':
-        alert('Это лето');
-        break;
-    case '9':
+    case '9': case '10': case '11':
         alert('Это осень');
-        break;
-    case '10':
-        alert('Это осень');
-        break;
-    case '11':
-        alert('Это осень');
-        break;
-    case '12':
-        alert('Это зима');
         break;
     default:
         alert('такого месяца не существует')
