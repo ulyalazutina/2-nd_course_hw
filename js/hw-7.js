bfcef0e3ee2a619463dfa555e6b0a81fee5e9d63
Ulyana Lazutina <ulyalazutina@yandex.ru>
2023-09-28 20:59:57 +0300

    Исправила работу

--- js/hw-7.js
+++ js/hw-7.js
@@ -6,8 +6,8 @@ console.log(str);
 //Задание 2
 const getArray = (array, stroke) => {
 
-    let newArray = [];
-    let regExp = new RegExp(`${stroke}+`, 'gi');
+    const newArray = [];
+    const regExp = new RegExp(`${stroke}+`, 'gi');
 
     array.forEach(element => {
         if (regExp.test(element)) {
@@ -15,29 +15,28 @@ const getArray = (array, stroke) => {
         }
     });
 
-    console.log(array);
-    console.log(newArray)
+    return newArray;
 }
-getArray(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко'); // ['кошка', 'комар']
-getArray(['яблоко', 'груша', 'гриб', 'огурец'], 'гру'); // ['груша']
-getArray(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино'); // []
+console.log(getArray(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко')); // ['кошка', 'комар']
+console.log(getArray(['яблоко', 'груша', 'гриб', 'огурец'], 'гру')); // ['груша']
+console.log(getArray(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино')); // []
 
 
 //Задание 3
-let num = 32.58884;
+const num = 32.58884;
 console.log('До меньшего целого - ' + Math.floor(num) +
-' До большего целого - ' + Math.ceil(num) +
-' До ближайшего целого - ' + Math.round(num));
+    ' До большего целого - ' + Math.ceil(num) +
+    ' До ближайшего целого - ' + Math.round(num));
 
 //Задание 4
-let numMin = Math.min(52, 53, 49, 77, 21, 32);
-let numMax = Math.max(52, 53, 49, 77, 21, 32);
+const numMin = Math.min(52, 53, 49, 77, 21, 32);
+const numMax = Math.max(52, 53, 49, 77, 21, 32);
 
 console.log(numMin, numMax);
 
 //Задание 5
 const getRandom = () => {
-    let numRandom = (Math.round(Math.random() * (10 - 1)) + 1);
+    const numRandom = (Math.round(Math.random() * (10 - 1)) + 1);
     console.log(numRandom)
 }
 
@@ -51,33 +50,33 @@ const getRandomArray = (maxValue) => {
     for (let i = 0; i < length; i++) {
         a.push(Math.round(Math.random() * maxValue));
     }
-    console.log(a);
+    return a;
 }
 
-getRandomArray(7);
+console.log(getRandomArray(7));
 
 //Задание 7
 const getRandomNum = (minValue, maxValue) => {
-    let numRandom = (Math.round(Math.random() * (maxValue - minValue)) + minValue);
-    console.log(numRandom)
+    const numRandom = (Math.round(Math.random() * (maxValue - minValue)) + minValue);
+    return numRandom;
 }
 
-getRandomNum(1, 10);
+console.log(getRandomNum(1, 10));
 
 //Задание 8 
-let currentDate = new Date();
-let anotherFormat = currentDate.toLocaleString();
+const currentDate = new Date();
+const anotherFormat = currentDate.toLocaleString();
 console.log(anotherFormat);
 
 //Задание 9
 currentDate.setDate(currentDate.getDate() + 73);
-let newFormat = currentDate.toLocaleDateString();
+const newFormat = currentDate.toLocaleDateString();
 console.log(newFormat);
 
 //Задание 10
 const getDate = (userDate) => {
 
-    let myDate = new Date(`${userDate}`);
+    const myDate = new Date(`${userDate}`);
 
     const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
     const month = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
@@ -96,19 +95,19 @@ const getDate = (userDate) => {
         second = "0" + second;
     }
 
-    let date = `Дата: ${myDate.getDate()} ${month[myDate.getMonth()]} ${myDate.getFullYear()} - это ${days[myDate.getDay()]} Время: ${hours}:${minutes}:${second}`;
+    const date = `Дата: ${myDate.getDate()} ${month[myDate.getMonth()]} ${myDate.getFullYear()} - это ${days[myDate.getDay()]} Время: ${hours}:${minutes}:${second}`;
 
-    console.log(date)
+    return date;
 }
 
-getDate("8/1/2023, 5:55:3");
+console.log(getDate("8/1/2023, 5:55:3"));
 
 const rememberWords = () => {
     let arr = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
     arr = arr.sort(() => Math.random() - 0.5);
     alert(arr);
-    let answerOne = prompt("Чему равнялся первый элемент массива?").toLowerCase();
-    let answerTwo = prompt("Чему равнялся последний элемент массива?").toLowerCase();
+    const answerOne = prompt("Чему равнялся первый элемент массива?").toLowerCase();
+    const answerTwo = prompt("Чему равнялся последний элемент массива?").toLowerCase();
     for (let i = 0; i < arr.length; i++) {
         if (arr[0].toLowerCase() == answerOne && arr.at(-1).toLowerCase() == answerTwo) {
             alert('Поздравляю! Вы угадали оба элемента');
